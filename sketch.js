009f716a41a0aa5dda78300bc0153d2860272f79
annagargantini <anna1.gargantini@mail.polimi.it>
2021-09-28 22:08:13 +0200

    Delivery A1

--- sketch.js
+++ sketch.js
@@ -3,11 +3,41 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  angleMode(DEGREES);
   // put setup code here
 }
 
 function draw() {
   // put drawing code here
-  background("red");
+background(220);
+translate(0, -70, 0);
+rotateX (60);
+noFill();
+
+let myColor = lerpColor(color("#FF008D"), color("#FFAD00"), frameCount/1080);
+stroke(myColor);
+
+rotate(frameCount/3);
+ for (var i = 0 ; i < 50 ; i ++) { // numero forme
+   beginShape()
+   for (var j = 0; j < 360 ; j += frameCount/3) { // numero lati
+     var rad = i * 7 
+     var x = rad * cos (j)
+     var y = rad * sin (j)
+     var z = sin (frameCount*5 + i * 10 ) * 50
+
+     vertex(x, y, z)
+   }
+
+
+   endShape(CLOSE)
+
+   
+   if (frameCount === 1080) {
+    noLoop();
+  }
+ 
 }
+}
+
